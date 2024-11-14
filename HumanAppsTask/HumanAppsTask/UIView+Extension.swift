45a57aa47fe5e78886138b2c079ff357f81e37cd
Egor <eg887or@gmail.com>
2024-11-14 11:09:21 +0300

    [+] добавил методы в extension UIView

--- HumanAppsTask/HumanAppsTask/UIView+Extension.swift
+++ HumanAppsTask/HumanAppsTask/UIView+Extension.swift
@@ -11,8 +11,20 @@ extension UIView {
     ///Adds subviews and sets the views property `translatesAutoresizingMaskIntoConstraints` to `false`.
     func addSubviews(_ views: UIView...) {
         for i in views {
-            i.translatesAutoresizingMaskIntoConstraints = false
             self.addSubview(i)
         }
+    }
+    
+    func setBorder(width: CGFloat, color: UIColor, cornerRadius: CGFloat = 0) {
+        layer.borderWidth = width
+        layer.borderColor = color.cgColor
+        layer.cornerRadius = cornerRadius
+        
+        clipsToBounds = true
+    }
+    
+    func moveToCenterInSuperview() {
+        guard let superview else { return }
+        center = CGPoint(x: superview.bounds.midX, y: superview.bounds.midY)
     }
 }
