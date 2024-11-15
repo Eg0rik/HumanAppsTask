048ffb55cc5662da8531e3e98dbb24754ed0694d
Egor <eg887or@gmail.com>
2024-11-15 12:09:26 +0300

    [+] наполнил HomeViewController фунекционалом

--- HumanAppsTask/HumanAppsTask/HomeViewController.swift
+++ HumanAppsTask/HumanAppsTask/HomeViewController.swift
@@ -15,46 +15,21 @@ class HomeViewController: UIViewController {
     //MARK: - Private properties
     private let photoPicker: PhotoPicker
     
-    private var height: CGFloat {
-        view.frame.height
-    }
-    
-    private var width: CGFloat {
-        view.frame.width
-    }
-    
     private let editedViewDefaultSize = CGSize(width: 200, height: 200)
+    private let editedViewDefaultBorderColor = UIColor.systemYellow
+    private let editedViewDefaultBorderWidth: CGFloat = 10
+    
     
     //MARK: - Views
-//    private lazy var rect: UIView = {
-//        let view = UIView(frame: .init(x: 30, y: 30, width: 100, height: 100))
-//        view.backgroundColor = .systemRed
-//        
-//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(longPressGesture))
-//        panGesture.maximumNumberOfTouches = 1
-//        
-//        view.addGestureRecognizer(panGesture)
-//        return view
-//    }()
-    
-    private lazy var addImageButtonImageView: UIImageView = {
-        let imageView = UIImageView(image: UIImage(systemName: "plus.circle"))
-        imageView.tintColor = .systemBlue
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    private lazy var addImageButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.addSubview(addImageButtonImageView)
-        button.addTarget(self, action: #selector(addImage), for: .touchUpInside)
-        button.isHidden = false
-        return button
-    }()
-    
-    private lazy var editedView: EditedView = {
-        let view = EditedView()
+    private lazy var editedView: EditedImageView = {
+        let view = EditedImageView()
         view.isHidden = true
+        return view
+    }()
+    
+    private lazy var topBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .brandLightGray
         return view
     }()
     
@@ -74,10 +49,30 @@ class HomeViewController: UIViewController {
         return control
     }()
     
+    private lazy var colorPicker: UIColorPickerViewController = {
+        let colorPicker = UIColorPickerViewController()
+        colorPicker.title = "Frame color"
+        colorPicker.delegate = self
+        colorPicker.modalPresentationStyle = .popover
+        return colorPicker
+    }()
+    
+    private lazy var borderWidthSlider: UISlider = {
+        let slider = UISlider()
+        slider.value = 16
+        slider.minimumValue = 0
+        slider.maximumValue = 20
+        slider.thumbTintColor = editedViewDefaultBorderColor
+        slider.minimumTrackTintColor = editedViewDefaultBorderColor
+        slider.addTarget(self, action: #selector(sliderValueDidChange), for: .allEvents)
+        return slider
+    }()
+    
     //MARK: - Life Cycle
     init(photoPicker: PhotoPicker) {
         self.photoPicker = photoPicker
         super.init(nibName: nil, bundle: nil)
+        setupTabBarItem()
     }
     
     required init?(coder: NSCoder) {
@@ -87,6 +82,8 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        setupNavigationBar()
+        deactivateEditPanel()
         setupConstraints()
         testSetup()
     }
@@ -97,60 +94,166 @@ class HomeViewController: UIViewController {
 //MARK: - Private methods
 private extension HomeViewController {
     func setupView() {
-        view.addSubview(addImageButton)
-        view.addSubviews(editedView, segmentedControlImageMode)
+        view.addSubviews(editedView, topBackgroundView, segmentedControlImageMode, borderWidthSlider)
         
         photoPicker.delegate = self
     }
     
-    func setupConstraints() {
-        addImageButton.snp.makeConstraints {
-            $0.center.equalToSuperview()
-            $0.width.height.equalTo(100)
-        }
+    func setupTabBarItem() {
+        tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "scribble"),
+            selectedImage: UIImage(systemName: "scribble.variable")
+        )
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        addImageButtonImageView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
+        alertVC.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
-        segmentedControlImageMode.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            $0.leading.trailing.equalToSuperview().inset(15)
-            $0.height.equalTo(50)
-        }
+        present(alertVC, animated: true)
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Home"
+    }
+    
+    func activateEditPanel() {
+        segmentedControlImageMode.isEnabled = true
+        setBarButtonWithEditingMenu()
+    }
+    
+    func deactivateEditPanel() {
+        segmentedControlImageMode.isEnabled = false
+        setPlussBarButton()
+    }
+    
+    func setBarButtonWithEditingMenu() {
+        let menu = UIMenu(title: "Options", children: [
+            UIAction(title: "Set a default size") { [weak self] action in
+                guard let self else { return }
+                
+                self.editedView.moveToCenterInSuperview()
+                self.editedView.transform = .identity
+            },
+            
+            UIAction(title: "Choose new frame color",image: UIImage(systemName: "pencil.tip")) { [weak self] action in
+                guard let self else { return }
+                
+                self.present(self.colorPicker, animated: true)
+            },
+            
+            UIAction(title: "Save the image",image: UIImage(systemName: "square.and.arrow.down.fill")) { [weak self] action in
+                self?.saveImage()
+            },
+            
+            UIAction(title: "Delete the image",image: UIImage(systemName: "trash"), attributes: .destructive) { [weak self] action in
+                self?.deleteImage()
+            }
+        ])
+        
+        let rightButton = UIBarButtonItem(
+            title: nil,
+            image: UIImage(systemName: "slider.horizontal.3"),
+            target: nil,
+            action: nil,
+            menu: menu)
+        
+        navigationItem.rightBarButtonItem = rightButton
+    }
+    
+    // Button to add image from photo library.
+    func setPlussBarButton() {
+        let rightButton = UIBarButtonItem(
+            title: nil,
+            image: .add,
+            target: self,
+            action: #selector(addImage))
+        
+        navigationItem.rightBarButtonItem = rightButton
     }
     
     @objc func addImage() {
         photoPicker.present()
     }
     
-    func setEditedImage(_ image: UIImage?) {
+    func setupConstraints() {
+        topBackgroundView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(borderWidthSlider.snp.bottom).offset(10)
+        }
         
+        segmentedControlImageMode.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.height.equalTo(40)
+        }
+        
+        borderWidthSlider.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.top.equalTo(segmentedControlImageMode.snp.bottom).offset(20)
+        }
+    }
+    
+    func setEditedImage(_ image: UIImage?) {
         editedView.setImage(image)
-        editedView.setBorder(width: 2, color: .systemYellow)
         editedView.moveToCenterInSuperview()
         editedView.isHidden = false
         editedView.bounds.size = editedViewDefaultSize
+        editedView.setBorder(width: editedViewDefaultBorderWidth, color: editedViewDefaultBorderColor)
         
-        addImageButton.isHidden = true
-        
-        segmentedControlImageMode.isEnabled = true
+        activateEditPanel()
     }
     
-    func setupNavigationItem() {
-        navigationItem.title = "Home"
+    func deleteImage() {
+        editedView.delteImage()
+        editedView.isHidden = true
+        
+        deactivateEditPanel()
     }
     
-    func testSetup() {
-        setEditedImage(.image1)
+    func saveImage() {
+        guard let image = editedView.getImage() else { return }
+        
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_ : didFinishSavingWithError: contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        let message = error?.localizedDescription ?? "success"
+        
+        showAlert(title: "Saving image to galery ends with:", message: message)
+    }
+    
+    @objc func sliderValueDidChange(_ sender: UISlider) {
+        editedView.layer.borderWidth = CGFloat(sender.value)
     }
 }
 
+//MARK: PhotoPickerDelegate
 extension HomeViewController: PhotoPickerDelegate {
     func picker(_ picker: any PhotoPicker, didSelect image: UIImage) {
         setEditedImage(image)
     }
-    
+}
+
+//MARK: UIColorPickerViewControllerDelegate
+extension HomeViewController: UIColorPickerViewControllerDelegate {
+    func colorPickerViewController(_ colorPicker: UIColorPickerViewController, didSelect: UIColor, continuously: Bool) {
+        colorPicker.dismiss(animated: true)
+        
+        editedView.layer.borderColor = didSelect.cgColor
+        
+        borderWidthSlider.thumbTintColor = didSelect
+        borderWidthSlider.minimumTrackTintColor = editedViewDefaultBorderColor
+    }
+}
+
+//FIXME: - For tests
+private extension HomeViewController {
+    func testSetup() {
+        setEditedImage(.image1)
+    }
 }
 
 #Preview {
