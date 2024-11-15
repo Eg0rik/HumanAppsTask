5ba8c6c5f60a8422c9cbfc6a3d539a0445ea8b56
Egor <eg887or@gmail.com>
2024-11-15 12:10:19 +0300

    [+] базовые методы для SettingsViewController

--- HumanAppsTask/HumanAppsTask/SettingsViewController.swift
+++ HumanAppsTask/HumanAppsTask/SettingsViewController.swift
@@ -7,6 +7,48 @@
 
 import UIKit
 
-final class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController {
     
+    //MARK: - Public properties
+    
+    //MARK: - Private properties
+    
+    //MARK: - Views
+    
+    //MARK: - Life Cycle
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        setupTabBarItem()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupNavigationBar()
+    }
+    
+    //MARK: - Public methods
+}
+
+//MARK: - Private methods
+private extension SettingsViewController {
+    func setupView() {
+        
+    }
+    
+    func setupTabBarItem() {
+        tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "gear.circle"),
+            selectedImage: UIImage(systemName: "gear.circle.fill")
+        )
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Settings"
+    }
 }
